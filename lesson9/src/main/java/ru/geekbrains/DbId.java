183f75403255c8e417602326fe8c5206cd1f724a
tobi-hana <eltishev2@gmail.com>
2023-10-18 00:13:20 +0300

    All readme.md for 1-9 lessons, task 9 is not finished

--- lesson9/src/main/java/ru/geekbrains/DbId.java
+++ lesson9/src/main/java/ru/geekbrains/DbId.java
@@ -1,12 +1,11 @@
 package ru.geekbrains;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
 
-@Target(ElementType.TYPE)
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface DbTable {
-    String name();
+public @interface DbId {
 }
---
+++ lesson9/src/main/java/ru/geekbrains/ReflectionRepository.java
@@ -0,0 +1,10 @@
+package ru.geekbrains;
+
+public class ReflectionRepository<T, Long>{
+    public void save(T type) {
+
+    }
+    public T findById(Long id) {
+        return null;
+    }
+}
